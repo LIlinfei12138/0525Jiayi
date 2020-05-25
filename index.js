0e187123bc1e57f850101b671e02d60a18b338bb
LIlinfei12138 <1318151255@qq.com>
2020-05-25 23:25:17 +0800

    first submit

---
+++ index.js
@@ -0,0 +1,32 @@
+// 第一二个a标签
+$firstTwoTagA = $(".four-data>div:lt(2)>a");
+// 第三四个a标签
+$thirdFourthTagA = $(".four-data>div:gt(1)>a");
+// 第一二个图片
+$firstTwoTagImg = $(".four-data>div:lt(2)>img");
+// 第三四个图片
+$thirdFourthTagImg = $(".four-data>div:gt(1)>img");
+console.log($firstTwoTagImg);
+$(window).scroll(function(){
+	$scrollTop = $(window).scrollTop();
+	if ($scrollTop <=1300){
+		// a标签
+		$firstTwoTagA.addClass("cur");
+		$thirdFourthTagA.addClass("cur");
+		// 图片
+		$firstTwoTagImg.addClass("cur");
+		$thirdFourthTagImg.addClass("cur");
+	}else if ($scrollTop >=1900){
+		// a标签
+		$thirdFourthTagA.removeClass("cur");
+		// 图片
+		$thirdFourthTagImg.removeClass("cur");
+	}else if ($scrollTop >=1300){
+		// a标签
+		$firstTwoTagA.removeClass("cur");
+		$thirdFourthTagA.addClass("cur");
+		// 图片
+		$firstTwoTagImg.removeClass("cur");
+		$thirdFourthTagImg.addClass("cur");
+	}
+});
